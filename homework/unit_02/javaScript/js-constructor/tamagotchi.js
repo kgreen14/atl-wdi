4653ff520cd0167ce90c85174ecc75ca436e1f00
kgreen14 <kgreen14@gmail.com>
2017-07-24 18:29:39 -0400

    last code wasn't working, updated code to work

--- homework/unit_02/javaScript/js-constructor/tamagotchi.js
+++ homework/unit_02/javaScript/js-constructor/tamagotchi.js
@@ -1,23 +1,25 @@
-console.log('tamagotchi file is loaded');
-
-class Tamagotchi {
-  constructor(name, creatureType){
-    this.name = name;
-    this.creatureType = creatureType;
+class Tamagotchi{
+  constructor(){
     this.foodInTummy = 10;
     this.restedness = 10;
     this.health = 10;
   }
 
-  cry() {
+  cry(){
     this.foodInTummy--;
-    console.log("WAAAH!!!!!!");
-    console.log(this.name + ' has current food in tummy = ' + this.foodInTummy);
-  };
-  
+    console.log("The Tamagotchi is crying!!! WAAAH!!!!!!");
+    console.log('current food in tummy: ' + this.foodInTummy);
+  }
+}
 
-var bob = new Tamagotchi('bob', 'fish');
-var tom = new Tamagotchi('tom', 'cat');
+
+var bob = new Tamagotchi();
+var tom = new Tamagotchi();
+
+
+console.log(bob);
+console.log(tom);
+
 
 bob.cry();
 tom.cry();
