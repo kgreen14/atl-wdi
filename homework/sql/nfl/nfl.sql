6c3180c3705917154c82c1b3a2896399eafeb598
kgreen14 <kgreen14@gmail.com>
2017-08-17 18:40:21 -0400

    NFL - Finished the nfl queries

--- homework/sql/nfl/nfl.sql
+++ homework/sql/nfl/nfl.sql
@@ -13,3 +13,15 @@
 -- 3.7- select round( avg(salary)) from players;
 
 -- 3.8- select name, position from players where salary > 10000000;
+
+-- 3.9- select name from players order by salary desc limit 1
+
+-- 3.10- select name, position from players order by salary asc limit 100;
+
+-- 3.11- select round( avg(salary)) from players where position = 'DE';
+
+-- 3.12- select name from players where team_id = 1;
+
+-- 3.13- select sum(salary) from players where team_id = 18;
+
+-- 3.14- select name, salary from players where team_id = 23 order by salary asc limit 55;
