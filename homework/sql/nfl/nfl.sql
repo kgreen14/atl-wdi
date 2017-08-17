ddbea6428cec79e3ca70ba346da2263a2453b67a
kgreen14 <kgreen14@gmail.com>
2017-08-17 18:03:37 -0400

    NFL - queried some NFL stuff

---
+++ homework/sql/nfl/nfl.sql
@@ -0,0 +1,7 @@
+-- 3.1- select * from teams;
+
+-- 3.2- select stadium, head_coach from teams where conference = 'NFC';
+
+-- 3.3- select head_coach from teams where conference = 'AFC' and division = 'South';
+
+-- 3.4- 1532
