2d7ed03ef37d101a3fec457c991b9819d56bae12
kgreen14 <kgreen14@gmail.com>
2017-08-17 18:23:46 -0400

    NFL - wow there's a lot of nfl data

--- homework/sql/nfl/nfl.sql
+++ homework/sql/nfl/nfl.sql
@@ -5,3 +5,11 @@
 -- 3.3- select head_coach from teams where conference = 'AFC' and division = 'South';
 
 -- 3.4- 1532
+
+-- 3.5- select name, head_coach from teams where conference = 'NFC' and division = 'North' or conference = 'AFC' and division = 'East';
+
+-- 3.6- select name from players order by salary desc limit 50;
+
+-- 3.7- select round( avg(salary)) from players;
+
+-- 3.8- select name, position from players where salary > 10000000;
